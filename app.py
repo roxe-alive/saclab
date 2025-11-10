3c17acff7df4ca4db8f28599eccf2b6b5d9db48a
DOT_007 <roxezzx@gmail.com>
2025-11-10 15:21:57 +0530

    update

--- app.py
+++ app.py
@@ -4,10 +4,15 @@ from flask import Flask, send_file
 app =  Flask(__name__)
 
 PDF_LINK = "PDFS/pypdf.pdf"
+PDF_LINK_1 = "PDFS/mongodb.pdf"
+
 @app.route("/")
 def render():
     return send_file(PDF_LINK)
 
+@app.route("/1")
+def mongo():
+    return send_file(PDF_LINK)
 
 @app.route("/health")
 def health_check():
